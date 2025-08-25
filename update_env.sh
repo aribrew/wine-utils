8fdc253c395f1081c95c182f51eb6ffe216add99
aribrew <arithesage@protonmail.com>
2025-08-25 21:00:31 +0200

    Added a new one for updating the installed scripts.

---
+++ update_env.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+copy()
+{
+    $SUDO cp $*
+}
+
+
+BASH_HELPERS="/opt/bin/bash_helpers.sh"
+
+if ! [[ -f "$BASH_HELPERS" ]]; 
+then
+    echo "Cannot find '$BASH_HELPERS'."
+    echo ""
+
+    exit 1
+    
+else
+    source "$BASH_HELPERS"
+fi
+
+
+
+
+if ! [[ -v WINE_ENV ]] && ! [[ -f "$WINE_ENV/.wine_env" ]];
+then
+    abort "WINE environment not loaded yet. Source .wine_env."
+fi
+
+
+SCRIPTS=$(realpath $(dirname $0))
+
+
+touch "$WINE_ENV/testing"
+
+if ! [[ "$?" == "0" ]];
+then
+    sudo touch "$WINE_ENV/testing"
+
+    if ! [[ "$?" == "0" ]];
+    then
+        abort "WTF... Cannot write in '$WINE_ENV'!"
+    else
+        export SUDO="sudo"
+        sudo rm "$WINE_ENV/testing"
+    fi
+else
+    export SUDO=""
+    rm "$WINE_ENV/testing"
+fi
+
+
+copy -r "$SCRIPTS/cmds" "$WINE_ENV/cmds"
+
+copy "$SCRIPTS/download_wine.sh" "$WINE_ENV/"
+copy "$SCRIPTS/install_wine.sh" "$WINE_ENV/"
+copy "$SCRIPTS/install_winetricks.sh" "$WINE_ENV/"
+
+copỳ "$SCRIPTS/.wine_env" "$WINE_ENV/"
+
+echo "WINE Utils scripts updated."
+echo ""
