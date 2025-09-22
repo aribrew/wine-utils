4271e0078a04c0199481a24746ecae762e27fc7d
aribrew <arithesage@protonmail.com>
2025-09-22 20:39:01 +0200

    Better now.

--- install_wine.sh
+++ install_wine.sh
@@ -31,42 +31,37 @@ install_wine_from()
         WINE_BRANCH=$(cat "$WINE_PATH/.wine_branch")
         WINE_VERSION=$(cat "$WINE_PATH/.wine_version")
 
-        if [[ -d "$WINE_ENV_PATH/$WINE_FOLDER" ]]; 
+        if [[ -d "$WINE_ENV/$WINE_FOLDER" ]]; 
         then
             abort "This WINE version is already installed."
         fi
 
         echo -n "Installing WINE $WINE_VERSION ($WINE_BRANCH) for $WINE_ARCH "
-        echo "in '$WINE_ENV_PATH' ..."
-        echo "--------------------------------------------------------------------"
+        echo "in '$WINE_ENV' ..."
+        echo "---------------------------------------------------------------"
 
-        if ! [[ -d "$WINE_ENV_PATH" ]]; 
+        if ! [[ -d "$WINE_ENV" ]]; 
         then
-            mktree "$WINE_ENV_PATH"
+            mktree "$WINE_ENV"
         fi
 
-        copy -r "$WINE_PATH" "$WINE_ENV_PATH"
+        copy -ru "$WINE_PATH" "$WINE_ENV/"
 
-        if [[ "$WINE_ARCH" == "i386" ]] && ! [[ -f "$WINE_ENV_PATH/.default_wine32" ]]; 
+        if [[ "$WINE_ARCH" == "i386" ]] && 
+         ! [[ -f "$WINE_ENV/.default_wine32" ]]; 
         then
-            echo "$WINE_ENV_PATH/$WINE_FOLDER" > "$WINE_ENV_PATH/.default_wine32"
+            echo "$WINE_ENV/$WINE_FOLDER" > "$WINE_ENV/.default_wine32"
 
-            echo "'$WINE_ENV_PATH/$WINE_FOLDER' is now the default for 32 bits."
+            echo "'$WINE_ENV/$WINE_FOLDER' is now the default for 32 bits."
             echo ""
 
-        elif [[ "$WINE_ARCH" == "amd64" ]] && ! [[ -f "$WINE_ENV_PATH/.default_wine64" ]]; 
+        elif [[ "$WINE_ARCH" == "amd64" ]] && 
+           ! [[ -f "$WINE_ENV/.default_wine64" ]]; 
         then
-            echo "$WINE_ENV_PATH/$WINE_FOLDER" > "$WINE_ENV_PATH/.default_wine64"
+            echo "$WINE_ENV/$WINE_FOLDER" > "$WINE_ENV/.default_wine64"
 
-            echo "'$WINE_ENV_PATH/$WINE_FOLDER' is now the default for 64 bits."
+            echo "'$WINE_ENV/$WINE_FOLDER' is now the default for 64 bits."
             echo ""
-        fi
-        
-        grep -q ".wine_env" "$HOME/.environment"
-
-        if ! [[ "$?" == "0" ]]; 
-        then
-            echo "source $WINE_ENV_PATH/.wine_env" >> "$HOME/.environment"
         fi
 
         echo "Done."
@@ -99,10 +94,10 @@ check_if_admin
 
 if [[ -v SUPER_USER ]];
 then
-    WINE_ENV_PATH="/opt/winenv"
-    SUDO="sudo"
+    export WINE_ENV="/opt/winenv"
+    export SUDO="sudo"
 else
-    WINE_ENV_PATH="$HOME/.local/bin/winenv"
+    export WINE_ENV="$HOME/.local/bin/winenv"
 fi
 
 
