95e946e50b428c93fab77c08e6277bb58bd3de41
aribrew <arithesage@protonmail.com>
2025-09-23 10:53:10 +0200

    Added scripts for easily enable DirectX 11 and 12 compatibility for the current WINE installation.

---
+++ enable_dx12_support.sh
@@ -0,0 +1,43 @@
+#1/bin/bash
+
+source bash_helpers.sh
+
+if ! [[ -v BASH_HELPERS_LOADED ]];
+then
+    echo -e "BASH Helpers not found in PATH. Install them first.\n"
+    exit 1
+fi
+
+
+SCRIPT_HOME=$(realpath $(dirname $0))
+
+
+if ! [[ -v WINE_ENV ]];
+then
+    abort "WINE environment has not been loaded yet."
+fi
+
+
+if ! [[ -v WINELOADER ]];
+then
+    abort "You need a WINE installation loaded before doing this."
+fi
+
+
+if [[ "$(which winetricks)" == "" ]];
+then
+    "$SCRIPT_HOME/install_winetricks.sh"
+
+    if ! [[ "$?" == "0" ]];
+    then
+        abort "Winetricks installation failed. Cannot continue."
+    fi
+fi
+
+
+echo ""
+echo "Installing VKD3D for DirectX 12 compatibility..."
+echo "------------------------------------------------"
+
+winetricks vkd3d
+
