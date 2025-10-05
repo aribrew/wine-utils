465040da0707205da992dbed62aa4c9622247aa2
aribrew <arithesage@protonmail.com>
2025-10-05 22:22:39 +0200

    Better this way. Only if no custom prefix, and no custom version, is given, the executable will be analyzed to check what is needed.

--- cmds/win_start.sh
+++ cmds/win_start.sh
@@ -12,6 +12,7 @@ fi
 usage()
 {
     echo -e "Usage: win_start.sh <executable> [args]\n"
+
     echo -e "To force a specific prefix or WINE version, export"
     echo -e "WINE_PREFIX_NAME and/or WINE_INSTALL_PATH.\n"
 }
@@ -26,6 +27,7 @@ fi
 
 EXEC=$(realpath "$1")
 
+
 if ! [[ "$EXEC" == "" ]] && [[ -f "$EXEC" ]];
 then
     if ! [[ -v WINE_ENV ]];
@@ -35,60 +37,83 @@ then
 
     ARGS=${@:2}
 
-    EXEC_TYPE=$(exec_type "$EXEC")
-
-    if [[ "$EXEC_TYPE" == "windows-i386" ]];
+    if [[ -v WINE_INSTALL_PATH ]];
     then
-        if ! [[ -f "$WINE_ENV/.default_wine32" ]];
+        if ! [[ -f "$WINE_INSTALL_PATH/.wine_version" ]];
         then
-            abort "Install a WINE version for 32 bits apps."
-        fi
-
-        if ! [[ -d "$HOME/.wine/dosdevices" ]];
-        then
-            abort "Install a WINE prefix for win32 apps."
-        fi
-
-        if ! [[ -v WINE_INSTALL_PATH ]];
-        then
-            . wine_load.sh $(cat "$WINE_ENV/.default_wine32");
+            abort "Invalid WINE installation in '$WINE_INSTALL_PATH'."
         else
-            if ! [[ -f "$WINE_INSTALL_PATH/.wine_version" ]];
-            then
-                abort "Invalid WINE installation in '$WINE_INSTALL_PATH'."
-            else
-                . wine_load.sh "$WINE_INSTALL_PATH"
-            fi
+            . wine_load.sh "$WINE_INSTALL_PATH"
         fi
+    fi
 
-        if ! [[ -v WINE_PREFIX_NAME ]];
-        then
-            . "$HOME/.wine/activate"
-        else
-            if ! [[ -f "$HOME/.local/share/wineprefix/$WINE_PREFIX_NAME" ]];
-            then
-                abort "Invalid WINE prefix '$WINE_PREFIX_NAME'."
-            else
-                . "$HOME/.local/share/wineprefix/$WINE_PREFIX_NAME/activate"
-            fi
-        fi
-        
-    elif [[ "$EXEC_TYPE" == "windows-amd64" ]];
+    if [[ -v WINE_PREFIX_NAME ]];
     then
-        if ! [[ -f "$WINE_ENV/.default_wine64" ]];
+        if ! [[ -f "$HOME/.local/share/wineprefix/$WINE_PREFIX_NAME" ]];
         then
-            abort "Install a WINE version for 64 bits apps."
+            abort "Invalid WINE prefix '$WINE_PREFIX_NAME'."
+        else
+            . "$HOME/.local/share/wineprefix/$WINE_PREFIX_NAME/activate"
         fi
+    fi
 
-        if ! [[ -d "$HOME/.wine64/dosdevices" ]];
+    if ! [[ -v WINE_PREFIX_NAME ]] && ! [[ -v WINE_INSTALL_PATH ]];
+    then
+        EXEC_TYPE=$(exec_type "$EXEC")
+
+        if [[ "$EXEC_TYPE" == "windows-i386" ]];
         then
-            abort "Install a WINE prefix for win64 apps."
-        fi
+            if ! [[ -f "$WINE_ENV/.default_wine32" ]];
+            then
+                abort "Install a WINE version for 32 bits apps."
+            fi
 
-        . wine_load.sh $(cat "$WINE_ENV/.default_wine64")
-        . "$HOME/.wine64/activate"
-    else
-        abort "The specified executable isn't a Windows 32/64 bit one."
+            if ! [[ -d "$HOME/.wine/dosdevices" ]];
+            then
+                abort "Install a WINE prefix for win32 apps."
+            fi
+
+            if ! [[ -v WINE_INSTALL_PATH ]];
+            then
+                . wine_load.sh $(cat "$WINE_ENV/.default_wine32");
+            else
+                if ! [[ -f "$WINE_INSTALL_PATH/.wine_version" ]];
+                then
+                    abort "Invalid WINE installation in '$WINE_INSTALL_PATH'."
+                else
+                    . wine_load.sh "$WINE_INSTALL_PATH"
+                fi
+            fi
+
+            if ! [[ -v WINE_PREFIX_NAME ]];
+            then
+                . "$HOME/.wine/activate"
+            else
+                if ! [[ -f "$HOME/.local/share/wineprefix/$WINE_PREFIX_NAME" ]];
+                then
+                    abort "Invalid WINE prefix '$WINE_PREFIX_NAME'."
+                else
+                    . "$HOME/.local/share/wineprefix/$WINE_PREFIX_NAME/activate"
+                fi
+            fi
+            
+        elif [[ "$EXEC_TYPE" == "windows-amd64" ]];
+        then
+            if ! [[ -f "$WINE_ENV/.default_wine64" ]];
+            then
+                abort "Install a WINE version for 64 bits apps."
+            fi
+
+            if ! [[ -d "$HOME/.wine64/dosdevices" ]];
+            then
+                abort "Install a WINE prefix for win64 apps."
+            fi
+
+            . wine_load.sh $(cat "$WINE_ENV/.default_wine64")
+            . "$HOME/.wine64/activate"
+        else
+            abort "The specified executable isn't a Windows 32/64 bit one."
+        fi
     fi
 
     if [[ -v WINELOADER ]] && [[ -v WINEPREFIX ]];
