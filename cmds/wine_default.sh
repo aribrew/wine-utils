93dc60ffbaf0c89c82f6d98bea0cd4fb62d7d933
aribrew <arithesage@protonmail.com>
2025-12-06 23:06:35 +0100

    This is needed too

---
+++ cmds/wine_default.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if ! [[ WINE_ENV ]];
+then
+    echo "No WINE environment loaded."
+    echo ""
+
+    exit 1
+fi
+
+
+if [[ "$1" == "" ]] || ! [[ -f "$1/.wine_version" ]];
+then
+    echo "No valid WINE installation provided."
+    echo ""
+
+    exit 1
+else
+    WINE_INSTALL="$1"
+
+    echo "$WINE_ENV/$WINE_INSTALL" > "$WINE_ENV/.default_wine"
+
+    echo "WINE installation at '$WINE_INSTALL' will now be the default one."
+    echo "It will be loaded for preparing new prefixes."
+    echo ""
+fi
