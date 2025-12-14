1cb2494ac59e7b9a645c63dd32c559ca6c4ad57f
aribrew <arithesage@protonmail.com>
2025-12-14 21:05:32 +0100

    This is going well, although testing is needed.

--- wine.sh
+++ wine.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 
+ACTIVATOR="#!/bin/bash"
+ACTIVATOR+=""
+ACTIVATOR+="export WINEPREFIX=WINEPREFIX_PLACEHOLDER"
+ACTIVATOR+="export WINEARCH=WINEARCH_PLACEHOLDER"
+ACTIVATOR+="export WIN_C=WINEPREFIX_PLACEHOLDER/drive_c"
+ACTIVATOR+="export WIN_D=WINEPREFIX_PLACEHOLDER/drive_d"
+ACTIVATOR+=""
+ACTIVATOR+=""
+ACTIVATOR+="echo \"WINE prefix '$WINEPREFIX' activated.\""
+ACTIVATOR+="echo \"Unset WINEDEBUG var to view errors and warnings.\""
+ACTIVATOR+="echo \"\""
+
+
 abort()
 {
     MESSAGE=$1
@@ -101,6 +114,40 @@ install_prefix_defaulter()
 }
 
 
+is_wine_installation()
+{
+	WINE_PATH="$1"
+
+	if [[ -d "$WINE_PATH" ]];
+	then
+	    WINESERVER=$(find "$WINE_PATH"/** -type f -name "wineserver")
+
+	    if [[ "$WINESERVER" == "" ]];
+	    then
+	        return 1
+	    fi
+
+	    return 0
+	fi
+}
+
+
+is_wine_prefix()
+{
+	if [[ -d "$1" ]];
+	then
+	    WINEPREFIX="$1"
+	    
+        if [[ -d "$WINEPREFIX/dosdevices" ]];
+        then
+            return 0
+        fi
+	fi
+
+	return 1
+}
+
+
 filext()
 {
     FULL_PATH=$1
@@ -114,27 +161,37 @@ filext()
 
 load_prefix()
 {
+    if [[ -d "$1" ]];
+    then
+        echo "TODO"
+    fi
+}
+
+
+load_required_prefix()
+{
     EXEC_TYPE="$1"
 
     if [[ "$EXEC_TYPE" == "windows-i386" ]];
     then
-        if ! [[ -d "$HOME/.wine" ]];
+        if [[ -d "$HOME/.wine" ]];
         then
-            setup_prefix "$WINE_PREFIXES/wine32" win32
+            source "$HOME/.wine/activate"
+        else
+            abort "No default 32 bit prefix found."
         fi
-
-        source "$HOME/.wine/activate"
         
     elif [[ "$EXEC_TYPE" == "windows-amd64" ]];
     then
-        if ! [[ -d "$HOME/.wine64" ]];
+        if [[ -d "$HOME/.wine64" ]];
         then
-            setup_prefix "$WINE_PREFIXES/wine64" win64
+            source "$HOME/.wine64/activate"
+        else
+            abort "No default 64 bit prefix found."
         fi
-
-        source "$HOME/.wine64/activate"
+        
     else
-        abort "The specified executable isn't a Windows 32/64 bit one."
+        abort "The specified executable isn't a Windows one."
     fi
 }
 
@@ -142,17 +199,29 @@ load_prefix()
 load_wine()
 {
     WINE_PATH="$1"
+
+    is_wine_installation "$WINE_PATH"
     
-   	WINESERVER=$(find "$WINE_PATH"/** -type f -name "wineserver")
-   
-   	if [[ "$WINESERVER" == "" ]];
+   	if ! [[ "$?" == "0" ]];
    	then
         abort "No WINE installation found at '$WINE_PATH'."
    	fi
 
 	if ! [[ -v WINEARCH ]];
 	then
-        abort "No WINE prefix loaded. Unable to check the required arch."
+        abort "No WINE prefix loaded. Load one first."
+	fi
+
+	if [[ -f "$WINE_PATH/.wine_branch" ]];
+	then
+        WINE_BRANCH=$(cat "$WINE_PATH/.wine_branch")
+	fi
+
+	if [[ -f "$WINE_PATH/.wine_version" ]];
+	then
+        WINE_VERSION="("
+        WINE_VERSION+=$(cat "$WINE_PATH/.wine_version")
+        WINE_VERSION+=")"
 	fi
 
 	export WINE_BINARIES=$(find "$WINE_PATH"/** -type d -name "bin")
@@ -175,6 +244,40 @@ load_wine()
 
     export WINE="$WINELOADER"
     export WINESERVER="$WINE_BINARIES/wineserver"
+
+    alias wine="$WINELOADER"
+    alias wine32="$WINE_BINARIES/wine"
+    alias wine64="$WINE_BINARIES/wine64"
+
+    alias wineboot="\"$WINELOADER\" \"$WINE_UTILS/wineboot.exe\""
+    alias winecfg="\"$WINELOADER\" \"$WINE_UTILS/winecfg.exe\""
+    alias winedump="\"$WINE_BINARIES/winedump\""
+    alias cmd="\"$WINELOADER\" \"$WINE_UTILS/cmd.exe\""
+    alias explorer="\"$WINELOADER\" \"$WINE_UTILS/winefile.exe\""
+    alias reg="\"$WINELOADER\" \"$WINE_UTILS/reg.exe\""
+    alias regedit="\"$WINELOADER\" \"$WINE_UTILS/regedit.exe\""
+
+    echo "Activated Wine $WINE_BRANCH $WINE_VERSION: $WINE_PATH"
+    echo ""
+    echo "You have available the following aliases: "
+    echo "- wine: Default WINE executable for the loaded prefix"
+    echo "- wine32: WINE executable for 32 bit software"
+    echo "- wine64: WINE executable for 64 bit software"
+    echo "- wineboot: Performs a 'reboot' of the loaded prefix."
+    echo "- explorer, reg, regedit: Launch these Windows programs." 
+    echo ""
+}
+
+
+set_default_wine()
+{
+    WINE_PATH="$1"
+    
+	echo "$WINE_PATH" > "$HOME/.default_wine"
+	            
+	echo -e "WINE installation at '$WINE_PATH' made the default one."
+	echo -e "It will be loaded for preparing new prefixes.\n"
+	echo ""
 }
 
 
@@ -182,6 +285,18 @@ setup_prefix()
 {
     export WINEPREFIX="$1"
     export WINEARCH="$2"
+
+    if ! [[ -v WINELOADER ]];
+    then
+        if ! [[ -f "$HOME/.default_wine" ]];
+        then
+            abort "No WINE installation defined as the default."
+        else
+            WINE_PATH=$(cat "$HOME/.default_wine")
+
+            load_wine "$WINE_PATH"
+        fi
+    fi
     
     echo "Initializing $WINEARCH prefix '$WINEPREFIX' ..."
     echo ""
@@ -221,17 +336,21 @@ usage()
 	echo -e "wine.sh <executable> [args]"
 	echo -e ": Executes a program with the default prefix."
 	echo -e ""
+    echo -e "wine.sh --set_default <WINE installation>"
+    echo -e ": Set this WINE installation as the default one."
+    echo -e "  This is required by 'setup_prefix' to preload it."
+    echo -e ""
 	echo -e "wine.sh --setup_prefix <prefix name> [win32|win64]"
 	echo -e ": Create a new prefix in ~/.local/share/wineprefixes."
 	echo -e "  The default architecture, if none is specified, is win64."
 	echo -e ""
+    echo -e "wine.sh --load <WINE installation"
+    echo -e ": Use with 'source' or '.'."
+    echo -e "  Activates the given WINE installation."
+    echo -e ""
 	echo -e "wine.sh --load_prefix <prefix name>"
 	echo -e ": Use with 'source' or '.'."
 	echo -e "  Loads the given prefix in the current environment."
-	echo -e ""
-	echo -e "wine.sh --use <WINE installation"
-	echo -e ": Use with 'source' or '.'."
-	echo -e "  Activates the given WINE installation."
 	echo -e ""
 	echo -e "wine.sh --download [branch] [version]"
 	echo -e ": Downloads WINE to ~/.local/bin/wine folder."
@@ -245,15 +364,79 @@ usage()
 
 
 
-if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]];
+export WINE_PREFIXES="$HOME/.local/share/wineprefixes"
+
+
+if [[ "$1" == "" ]] || [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]];
 then
     usage
     abort
 fi
 
 
+if [[ "$1" == "--set_default" ]];
+then
+    if ! [[ "$2" == "" ]];
+    then
+        WINE_PATH="$2"
+
+        is_wine_installation "$WINE_PATH"
+
+        if [[ "$?" == "0" ]];
+        then
+            set_default_wine "$WINE_PATH"
+        fi
+    fi
+    
+elif [[ "$1" == "--setup_prefix" ]];
+then
+    echo "TODO"    
+
+elif [[ "$1" == "--load" ]];
+then
+    if ! [[ "$2" == "" ]];
+    then
+        WINE_PATH="$2"
+        
+        is_wine_installation "$WINE_PATH"
+        
+        if [[ "$?" == "0" ]];
+        then
+            load_wine "$WINE_PATH"
+        fi
+    fi
+    
+elif [[ "$1" == "--load_prefix" ]];
+then
+    if ! [[ "$2" == "" ]];
+    then
+        WINEPREFIX="$2"
+
+        is_wine_prefix "$WINEPREFIX"
+
+        if [[ "$?" == "0" ]];
+        then
+            load_prefix "$WINEPREFIX"
+        fi
+    fi
+
+elif [[ "$1" == "--download" ]];
+then
+    echo "TODO"
+
+elif [[ "$1" == "--autoload" ]];
+then
+    echo "TODO"
+
+elif [[ "$1" == "--autoload_prefix" ]];
+then
+    echo "TODO"
+else
+    echo "TODO"
+fi
+
+
 export WINE_PATH=$(realpath $(dirname "$0"))
-export WINE_PREFIXES="$HOME/.local/share/wineprefixes"
 
 
 if ! [[ "$WINESERVER" == "" ]];
