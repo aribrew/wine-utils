49a25a319480d9091a1f980703fbecccbaada267
aribrew <arithesage@protonmail.com>
2026-09-16 13:53:30 +0200

    Adding support for the gogg apps in Linux and MacOS. Also, you can provide the game URL from the GOG store to fetch its ID.

--- gog/gog_game_id.sh
+++ gog/gog_game_id.sh
@@ -5,16 +5,35 @@ SCRIPT_HOME=$(realpath $(dirname $0))
 
 usage()
 {
-    echo "Usage: gog_game_id.sh <game name>"
+    echo "Usage: gog_game_id.sh <game name|URL>"
     echo ""
     echo "The game name must be one of the entries of the catalog."
+    echo "You can provide the full name or part of it."
+    echo "In the second case, only the last match is taken."
+    echo ""
+    echo "You can also provide the URL of the game in the GOG store."
     echo ""
 }
 
 
-if ! [[ -f "$SCRIPT_HOME/gogg" ]];
+OS=$(uname -a)
+
+if [[ $OS == Linux* ]];
 then
-    echo -e "Cannot find gogg executable. Must be along the script.\n"
+    GOGG="lgogg"
+
+elif [[ $OS == Darwin ]];
+then
+    GOGG="mgogg"
+else
+    echo -e "No Linux or Mac... what the hell is this?\n"
+    exit -1
+fi
+
+
+if ! [[ -f "$SCRIPT_HOME/$GOGG" ]];
+then
+    echo -e "Cannot find $GOGG executable. Must be along the script.\n"
     exit 1
 fi
 
@@ -30,6 +49,18 @@ export PATH=$SCRIPT_HOME:$PATH
 
 
 GAME_NAME="$1"
-GAME_ID=$(gogg catalogue search "$GAME_NAME" 2> /dev/null | grep "$GAME_NAME" | tail -1 | cut -d "|" -f 3 | xargs)
+
+if [[ $GAME_NAME == https* ]];
+then
+    GAME_URL="$GAME_NAME"
+    GAME_ID=$(curl $GAME_URL | grep '"sku":')
+
+    if ! [[ "$GAME_ID" == "" ]];
+    then
+        GAME_ID=$(echo "$GAME_ID" | xargs | sed 's/,/g' | cut -d ' ' -f 2)
+    fi
+else
+    GAME_ID=$($GOGG catalogue search "$GAME_NAME" 2> /dev/null | grep "$GAME_NAME" | tail -1 | cut -d "|" -f 3 | xargs)
+fi
 
 echo "$GAME_ID"
